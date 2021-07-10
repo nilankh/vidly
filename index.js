37a066f295c41b918af5717c9bd6212f197e69f4
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-10 22:24:07 +0530

    customer api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers')
 const express = require('express');
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/vidly')
     .catch(err => console.error('Could not connect to MongoDB...'))
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
