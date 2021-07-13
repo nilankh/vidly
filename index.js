cc2d526c1e072290cee300c31af77f9e8a296015
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-13 12:33:56 +0530

    transactions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers')
+const rentals = require('./routes/rentals')
+const movies = require('./routes/movies')
 const express = require('express');
 const app = express();
 
@@ -11,8 +13,10 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies)
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
-    console.log(`App listening on ${port} 3000!`);
+    console.log(`App listening on ${port} `);
 });
