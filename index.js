5d7cf38423aa75615731288f61669bb68b4a08cc
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-13 12:58:04 +0530

    modified and added objectid

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi)
+const mongoose = require('mongoose')
+const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const rentals = require('./routes/rentals')
 const movies = require('./routes/movies')
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/vidly')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'))
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
+mongoose
+  .connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.error('Could not connect to MongoDB...'))
+app.use(express.json())
+app.use('/api/genres', genres)
+app.use('/api/customers', customers)
 app.use('/api/movies', movies)
-app.use('/api/rentals', rentals);
+app.use('/api/rentals', rentals)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(3000, () => {
-    console.log(`App listening on ${port} `);
-});
+  console.log(`App listening on ${port} `)
+})
